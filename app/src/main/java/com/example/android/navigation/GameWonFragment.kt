5ef2cf44e37228056afb4f28ca379d0daba411d6
aditya <aditya003.ay@gmail.com>
2021-11-22 17:05:30 +0530

    passing parameters and actions to GameFragment.kt to GameWonFragment.kt

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,9 +20,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.android.navigation.databinding.FragmentGameWonBinding
 
 
@@ -32,9 +35,14 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
-        binding.nextMatchButton.setOnClickListener(
-                Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
-        )
+        binding.nextMatchButton.setOnClickListener{ v:View->
+            v.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+
+        }
+        val args: GameWonFragmentArgs by navArgs()
+        Toast.makeText(context , "NumCorrect:${args.numCorrect}," +
+                "NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 }
